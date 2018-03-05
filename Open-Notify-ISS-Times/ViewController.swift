45c863f113bc6c7be3f3141199924319a3ac4c7a
Shaik Akbar Basha <shbasha5@in.ibm.com>
2018-03-06 00:23:39 +0530

    Initial Commit

---
+++ Open-Notify-ISS-Times/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Open-Notify-ISS-Times
+//
+//  Created by Shaik Akbar Basha on 3/6/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
